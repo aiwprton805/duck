c992c6e4006b49c492408088e00e055627442f6a
aiwprton805 <aiwprton805@mail.ru>
2019-10-20 02:14:32 +0300

    remove RandomStringUtils import

--- src/main/java/com/magistr/duck/mvc/ProfileRestController.java
+++ src/main/java/com/magistr/duck/mvc/ProfileRestController.java
@@ -8,7 +8,6 @@ import com.magistr.duck.dto.UserProfile;
 import com.magistr.duck.entity.*;
 import com.magistr.duck.security.SecurityService;
 import com.magistr.duck.service.*;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
