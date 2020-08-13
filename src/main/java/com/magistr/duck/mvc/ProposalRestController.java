9774715a2159dc43e9a4b5f527ba694cacc81ec3
Violetta Makarycheva <aezakmi805@mail.ru>
2020-08-13 17:24:01 +0300

    Add ProposalRestController

---
+++ src/main/java/com/magistr/duck/mvc/ProposalRestController.java
@@ -0,0 +1,57 @@
+package com.magistr.duck.mvc;
+
+import com.magistr.duck.common.enums.ProposalStatus;
+import com.magistr.duck.entity.Profile;
+import com.magistr.duck.entity.Proposal;
+import com.magistr.duck.service.ProfileService;
+import com.magistr.duck.service.ProposalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/proposal")
+public class ProposalRestController {
+    private final ProposalService proposalService;
+    private final ProfileService profileService;
+
+    @Autowired
+    public ProposalRestController(ProposalService proposalService, ProfileService profileService) {
+        this.proposalService = proposalService;
+        this.profileService = profileService;
+    }
+
+    @GetMapping("/proposals/start/{start}/length/{length}")
+    public Map<String, Object> getProposals(@PathVariable Integer start,
+                                            @PathVariable Integer length,
+                                            Authentication authentication) {
+        List<String> roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        List<Proposal> proposals;
+        if (roles.contains("admin") && roles.contains("lector")) {
+            proposals = proposalService.getProposalsByStatus(ProposalStatus.NEW);
+            proposals.addAll(proposalService.getProposalsByLector(profileService.getProfile(
+                    (Principal) authentication.getPrincipal()).orElseGet(Profile::new)));
+        } else if (roles.contains("admin")) {
+            proposals = proposalService.getProposalsByStatus(ProposalStatus.NEW);
+        } else if (roles.contains("lector")) {
+            proposals = proposalService.getProposalsByLector(profileService.getProfile(
+                    (Principal) authentication.getPrincipal()).orElseGet(Profile::new));
+        } else {
+            proposals = new ArrayList<>(0);
+        }
+        Integer proposalsSize = proposals.size();
+        return Map.of("data", proposals.stream().skip(start).limit(length).collect(Collectors.toList()),
+                "size", proposalsSize);
+    }
+}
+//class
